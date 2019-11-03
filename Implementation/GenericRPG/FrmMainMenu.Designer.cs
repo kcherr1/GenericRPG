682071743fcead72c065d1e0d03826838999e446
Gladianous <cathibodeaux98@Gmail.com>
2019-11-03 15:16:35 -0600

    Load Menu Button
    -Added an empty button to the main menu

--- Implementation/GenericRPG/FrmMainMenu.Designer.cs
+++ Implementation/GenericRPG/FrmMainMenu.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(337, 121);
+            this.button2.Location = new System.Drawing.Point(337, 32);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(148, 66);
             this.button2.TabIndex = 1;
@@ -52,6 +53,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(337, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Load Game";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // FrmMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -59,6 +69,7 @@
             this.BackgroundImage = global::GenericRPG.Properties.Resources.Main_Menu;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Name = "FrmMainMenu";
@@ -71,5 +82,6 @@
         #endregion
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
